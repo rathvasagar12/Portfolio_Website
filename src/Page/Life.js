7ef9f00dc4534ecc00fa79a47cc93fef6e55699f
rathvasagar12 <rathwasagar007@gmail.com>
2026-07-05 13:16:31 +0530

    mad some change

--- src/Page/Life.js
+++ src/Page/Life.js
@@ -6,7 +6,7 @@ import img7 from '../Img/img7.jpeg';
 import img72 from '../Img/img72.png';
 import img74 from '../Img/img74.jpeg';
 import img75 from '../Img/img75.jpeg';
-import imgtrip from '../Img/imgtrip.jpg'
+
 function Life() {
     return (
         <div className="portfolio-page-content">
@@ -25,9 +25,7 @@ function Life() {
                         <div className="life-grid-item">
                             <img src={img5} alt="img" />
                         </div>
-                        {/* <div className="life-grid-item">
-                            <img src={imgtrip} alt="img" />
-                        </div> */}
+
                     </div>
 
                     {/* Column 2 */}
